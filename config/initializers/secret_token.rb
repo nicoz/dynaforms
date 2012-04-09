63f6466310792110cb2ca723190a3f5f32900b15
Nicolas Zuasti <zonic@outland.(none)>
2012-04-09 16:56:44 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dynaforms::Application.config.secret_token = '0bba9d2e15787b80bf6de3811d8b9722a9838316804ce24f22a68e1ffc6275f0d02ddc34d273207aa8f255cf55bcbf3a623d2f3939d9d295d2951d2355184efe'
